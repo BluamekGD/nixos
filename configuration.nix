c3a81509691b5a6e3798791c1b432e02ad20279b
bluamek <bartosz.nicely@gmail.com>
2026-05-11 15:30:21 +0200

    i forgot what i changed

--- configuration.nix
+++ configuration.nix
@@ -39,27 +39,8 @@
     initialPassword = "changeme";
   };
 
-  # zsh (peak shell)
-  programs.zsh = {
-    enable = true;
-
-    # I'd rather die + obligatory flex
-    initContent = "zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'; nixfetch";
-
-    # Aliases
-    shellAliases = {
-      nixfetch = "fastfetch --kitty-direct /etc/nixos/resources/logo.png";
-      nixos-update = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-    };
-
-    # History
-    history = {
-      size = 1000;
-      save = 1000;
-      ignoreDups = true;
-      share = true;
-    };
-  };
+  # zsh
+  programs.zsh.enable = true;
 
   # System packages
   nixpkgs.config.allowUnfree = true;
