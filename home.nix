cf9e6d781880b4ed647ad67e11be6918adf2b644
bluamek <bartosz.nicely@gmail.com>
2026-05-13 22:35:04 +0200

    not hallucinating anymore + 24 is gigantic bro

--- home.nix
+++ home.nix
@@ -17,7 +17,7 @@
   home.pointerCursor = {
     name = "Bibata-Modern-Classic";
     package = pkgs.bibata-cursors;
-    size = 24;
+    size = 16;
     gtk.enable = true;
     x11.enable = true;
   };
