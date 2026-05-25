689480151dad8894b0e01dfa95f0f329a2f857ee
bluamek <bartosz.nicely@gmail.com>
2026-05-25 21:13:40 +0200

    not sure if the warnings correct

--- home.nix
+++ home.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "bartek";
   home.homeDirectory = "/home/bartek";
 
@@ -44,36 +48,35 @@
   wayland.windowManager.sway = {
     enable = true;
     xwayland = true;
-    config = rec {
-      
+    config = {
       # Window borders and gaps
       gaps = {
         inner = 4;
-	outer = 10;
+        outer = 10;
       };
       window = {
         border = 6;
-	titlebar = false;
+        titlebar = false;
       };
       floating = {
         border = 6;
-	titlebar = false;
+        titlebar = false;
       };
       colors = {
         focused = {
           border = "#ebdbb2";
-	  background = "#ebdbb2";
-	  text = "#1e1e2e";
-	  indicator = "#ebdbb2";
-	  childBorder = "#ebdbb2";
-	};
-	unfocused = {
+          background = "#ebdbb2";
+          text = "#1e1e2e";
+          indicator = "#ebdbb2";
+          childBorder = "#ebdbb2";
+        };
+        unfocused = {
           border = "#1d2021";
-	  background = "#1d2021";
-	  text = "#1e1e2e";
-	  indicator = "#1d2021";
-	  childBorder = "#1d2021";
-	};
+          background = "#1d2021";
+          text = "#1e1e2e";
+          indicator = "#1d2021";
+          childBorder = "#1d2021";
+        };
       };
 
       # XCursor
@@ -87,25 +90,25 @@
       input = {
         "type:pointer" = {
           accel_profile = "flat";
-	  # "The Devil's Absolute Nightmare" Removed, Yay!!!!!!
+          # "The Devil's Absolute Nightmare" Removed, Yay!!!!!!
 
-	  # Sensitivity
-	  pointer_accel = "0";
-	};
+          # Sensitivity
+          pointer_accel = "0";
+        };
       };
-      
+
       # Preferences
       modifier = "Mod4";
       terminal = "kitty --title kitty";
       menu = "fuzzel";
       defaultWorkspace = "1";
-      
+
       # Waybar and background
       startup = [
         {command = "waybar";}
-	{command = "swaybg -o eDP-1 -i /etc/nixos/resources/swaybg.png -m fill";}
-	# The best solution to a problem is usually the easiest. -Ellen McLain (GLaDOS), 2011
-	{command = "swaymsg workspace number 1";}
+        {command = "swaybg -o eDP-1 -i /etc/nixos/resources/swaybg.png -m fill";}
+        # The best solution to a problem is usually the easiest. -Ellen McLain (GLaDOS), 2011
+        {command = "swaymsg workspace number 1";}
       ];
 
       # I have waybar bro
@@ -114,35 +117,35 @@
       keybindings = let
         # Read from preferences
         mod = config.wayland.windowManager.sway.config.modifier;
-	terminal = config.wayland.windowManager.sway.config.terminal;
-	menu = config.wayland.windowManager.sway.config.menu;
+        terminal = config.wayland.windowManager.sway.config.terminal;
+        menu = config.wayland.windowManager.sway.config.menu;
       in {
         # ------------------ Keybinds ------------------
-	
-	# ------ Apps ------
-        
-	"${mod}+Return" = "exec ${terminal}";
+
+        # ------ Apps ------
+
+        "${mod}+Return" = "exec ${terminal}";
         "${mod}+Space" = "exec ${menu}";
         "${mod}+q" = "kill";
 
-	# ------ Login ------
+        # ------ Login ------
 
-	"${mod}+Shift+L" = "exec swaymsg exit";
-	"${mod}+L" = "exec swaylock";
+        "${mod}+Shift+L" = "exec swaymsg exit";
+        "${mod}+L" = "exec swaylock";
 
-	# ------------ Window management ------------
+        # ------------ Window management ------------
 
         # ------ Movement ------
 
-	"${mod}+Left" = "focus left";
-	"${mod}+Right" = "focus right";
-	"${mod}+Up" = "focus up";
-	"${mod}+Down" = "focus down";
+        "${mod}+Left" = "focus left";
+        "${mod}+Right" = "focus right";
+        "${mod}+Up" = "focus up";
+        "${mod}+Down" = "focus down";
 
-	# ------ Workspaces ------
+        # ------ Workspaces ------
 
-	"${mod}+1" = "workspace number 1";
-	"${mod}+2" = "workspace number 2";
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
         "${mod}+3" = "workspace number 3";
         "${mod}+4" = "workspace number 4";
         "${mod}+5" = "workspace number 5";
@@ -151,19 +154,19 @@
         "${mod}+8" = "workspace number 8";
         "${mod}+9" = "workspace number 9";
         "${mod}+0" = "workspace number 10";
-	"${mod}+Shift+1" = "move container to workspace number 1";
-	"${mod}+Shift+2" = "move container to workspace number 2";
-	"${mod}+Shift+3" = "move container to workspace number 3";
-	"${mod}+Shift+4" = "move container to workspace number 4";
-	"${mod}+Shift+5" = "move container to workspace number 5";
-	"${mod}+Shift+6" = "move container to workspace number 6";
-	"${mod}+Shift+7" = "move container to workspace number 7";
-	"${mod}+Shift+8" = "move container to workspace number 8";
-	"${mod}+Shift+9" = "move container to workspace number 9";
-	"${mod}+Shift+0" = "move container to workspace number 10";
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+        "${mod}+Shift+6" = "move container to workspace number 6";
+        "${mod}+Shift+7" = "move container to workspace number 7";
+        "${mod}+Shift+8" = "move container to workspace number 8";
+        "${mod}+Shift+9" = "move container to workspace number 9";
+        "${mod}+Shift+0" = "move container to workspace number 10";
 
-	"${mod}+S" = "scratchpad show";
-	"${mod}+Shift+S" = "move scratchpad";
+        "${mod}+S" = "scratchpad show";
+        "${mod}+Shift+S" = "move scratchpad";
       };
     };
   };
